c69e7b69ede832ed122e5ccb07e2c97a30c7fc8b
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-23 13:53:12 +0900

    add: 필요한 command 추상화하여 commandHandler에 등록

--- src/command-manager.ts
+++ src/command-manager.ts
@@ -1,8 +1,33 @@
 import { Message } from "discord.js";
 
-const COMMAND_REGEX: RegExp = /^@@[a-zA-Z]+/;
+const COMMAND_REGEX: RegExp = /^!![a-zA-Z]+/;
 
 export const isCommand = (msgContent: string) => {
   return COMMAND_REGEX.test(msgContent);
 };
-export const commandHandler = (msg: Message) => {};
+export const commandHandler = (msg: Message) => {
+  switch (msg.content) {
+    case "!!play":
+      msg.channel.send("this is play music right now function");
+      break;
+    case "!!add":
+      msg.channel.send("this is add music into queue function");
+      break;
+    case "!!insert":
+      msg.channel.send("this is insert music into queue function");
+      break;
+    case "!!delete":
+      msg.channel.send("this is delete music by number function");
+    case "!!show":
+      msg.channel.send("this is show play list function");
+      break;
+    case "!!help":
+      msg.channel.send("this is help function");
+      break;
+    case "!!clear":
+      msg.channel.send("this is clear music queue function");
+      break;
+    default:
+      msg.channel.send("this is help function");
+  }
+};
